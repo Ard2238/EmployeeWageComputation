9a813de9fcfa599bc1a98b3518f69d309d665b19
Ard2238 <abhishekrdas28@gmail.com>
2020-09-28 03:35:45 +0530

    Refactoring Code to include class methods and variables

--- src/empwagecomp/EmpWageComp.java
+++ src/empwagecomp/EmpWageComp.java
@@ -2,11 +2,42 @@ package empwagecomp;
 
 public class EmpWageComp {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	static final int present_fulltime = 1;
+	static final int present_parttime = 2;
+	static final int total_working_days = 20;
+	static final int total_working_hours = 100;
+	
+	public void calculateWage() {
+		int work_hours = 0;
+		int perhour_wage = 20;	
+		int total_wages = 0, total_hours = 0, total_days = 0;	
 		
-		System.out.println("Welcome to Employee Wage Computation Program");
+		while (total_days < total_working_days && total_hours <= total_working_hours)
+		{
+			int checkEmp = (int) (Math.random() * 10) % 3;
+			switch (checkEmp){
+				case present_fulltime:
+					work_hours = 8;
+					break;
+				case present_parttime:
+					work_hours = 4;
+					break;
+				default:
+					work_hours = 0;		
+			}
 
+			total_hours += work_hours;
+			total_days++;
+
+			System.out.println("Current Day Hours: " + work_hours+ "   Total hours: " + total_hours);
+		}
+
+		System.out.println("The Total Monthly wages are " + total_hours * perhour_wage);
+		
 	}
-
+	public static void main(String args[]){
+		
+		EmpWageComp ewc= new EmpWageComp();
+		ewc.calculateWage();		
+	}
 }
