e717ad11e4d8917f3525cbb55976c3fcc2971b46
Ard2238 <abhishekrdas28@gmail.com>
2020-09-28 01:33:09 +0530

    Added to Calculate Part Time Wages

--- src/empwagecomp/EmpWageComp.java
+++ src/empwagecomp/EmpWageComp.java
@@ -5,7 +5,19 @@ public class EmpWageComp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Welcome to Employee Wage Computation Program");
+		int present_fulltime= 1, present_parttime=2, fullday_hours= 0;
+		int perhour_wage = 20;
+		int checkEmp = (int) (Math.random() * 10) % 3;
+		
+		if( checkEmp == present_parttime){
+			fullday_hours = 4;
+		}
+		else if ( checkEmp == present_fulltime){
+			fullday_hours = 8;
+		}
+		
+		int wages = perhour_wage * fullday_hours;
+		System.out.println("The Employee wages are " + wages);
 
 	}
 
