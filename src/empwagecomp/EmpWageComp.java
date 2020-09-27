4fead8e08174afdf78e952f46e4edab26ea20f9b
Ard2238 <abhishekrdas28@gmail.com>
2020-09-28 01:36:23 +0530

    Added to Check Switch Case Implementation

--- src/empwagecomp/EmpWageComp.java
+++ src/empwagecomp/EmpWageComp.java
@@ -1,11 +1,28 @@
 package empwagecomp;
 
 public class EmpWageComp {
+	
+		static final int present_fulltime = 1;
+		static final int present_parttime = 2;
+		public static void main(String args[]){
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		System.out.println("Welcome to Employee Wage Computation Program");
+			int work_hours = 0;
+			int perhour_wage = 20;
+			int checkEmp = (int) (Math.random() * 10) % 3;
+			
+			switch (checkEmp){
+				case present_fulltime:
+					work_hours = 8;
+					break;
+				case present_parttime:
+					work_hours = 4;
+					break;
+				default:
+					work_hours = 0;		
+			}
+			
+			int wages = perhour_wage * work_hours;
+			System.out.println("The Employee wages are " + wages);
 
 	}
 
