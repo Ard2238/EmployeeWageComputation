299cab6736fceb4329454ea7e81fd6ad41287e1a
Ard2238 <abhishekrdas28@gmail.com>
2020-09-28 01:38:43 +0530

    Added to Calculate Monthly Wages

--- src/empwagecomp/EmpWageComp.java
+++ src/empwagecomp/EmpWageComp.java
@@ -2,10 +2,32 @@ package empwagecomp;
 
 public class EmpWageComp {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		System.out.println("Welcome to Employee Wage Computation Program");
+		static final int present_fulltime = 1;
+		static final int present_parttime = 2;
+		static final int total_working_days = 20;
+		public static void main(String args[]){
+
+			int work_hours = 0;
+			int perhour_wage = 20;	
+			int total_wages = 0;	
+			
+			for(int i=0; i<total_working_days; i++){
+				int checkEmp = (int) (Math.random() * 10) % 3;
+				switch (checkEmp){
+					case present_fulltime:
+						work_hours = 8;
+						break;
+					case present_parttime:
+						work_hours = 4;
+						break;
+					default:
+						work_hours = 0;		
+				}
+			
+				int wages = perhour_wage * work_hours;
+	 			total_wages +=  wages;
+			}
+			System.out.println("The Total Monthly wages are " + total_wages);
 
 	}
 
