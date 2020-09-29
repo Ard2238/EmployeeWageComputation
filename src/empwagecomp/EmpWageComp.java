733ca1ed62e110c2d724264368834e5414987551
Ard2238 <abhishekrdas28@gmail.com>
2020-09-29 23:01:47 +0530

    Added to implement multiple companies functionality

--- src/empwagecomp/EmpWageComp.java
+++ src/empwagecomp/EmpWageComp.java
@@ -1,20 +1,15 @@
 package empwagecomp;
 
+import java.util.HashSet;
+import java.util.Scanner;
+
 public class EmpWageComp {
 
 	static final int present_fulltime = 1;
 	static final int present_parttime = 2;
-	private int total_working_days;
-	private int total_working_hours;
-	private int perhour_wage;
+	private static HashSet<EmpWageComp> set = new HashSet<EmpWageComp>();
 	
-	public EmpWageComp(int total_working_days, int total_working_hours, int perhour_wage) {
-		this.total_working_days = total_working_days;
-		this.total_working_hours = total_working_hours;
-		this.perhour_wage = perhour_wage;
-	}
-	
-	public void calculateWage() {
+	public void calculateWage(int total_working_days, int total_working_hours, int perhour_wage) {
 		int work_hours = 0;	
 		int total_wages = 0, total_hours = 0, total_days = 0;	
 		
@@ -43,8 +38,28 @@ public class EmpWageComp {
 	}
 	public static void main(String args[]){
 		
-		EmpWageComp ewc= new EmpWageComp(20,100,20);
-		ewc.calculateWage();		
+		Scanner sc = new Scanner(System.in);
+		int choice = 1;
+		
+		while(choice != 0) {
+			System.out.println("Do you want to compute wages for another company? \n0. No \n1. Yes");
+			choice = sc.nextInt();
+			
+			if(choice == 1) {
+				System.out.println("Enter the total number of working days:");
+				int t_days = sc.nextInt();
+				System.out.println("Enter the total number of working hours:");
+				int t_hrs = sc.nextInt();
+				System.out.println("Enter the wage per hour:");
+				int ph_wage = sc.nextInt();
+				
+				EmpWageComp comp1= new EmpWageComp();
+				comp1.calculateWage(t_days,t_hrs,ph_wage);
+				set.add(comp1);
+			}
+		}
+		sc.close();
+		return;
 	}
 }
 
