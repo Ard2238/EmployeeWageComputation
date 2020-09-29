0291658cf52df8f7115b7a405fc9fccebf1fb287
Ard2238 <abhishekrdas28@gmail.com>
2020-09-29 23:13:20 +0530

    Added to save total wages for each company

--- src/empwagecomp/EmpWageComp.java
+++ src/empwagecomp/EmpWageComp.java
@@ -4,14 +4,28 @@ import java.util.HashSet;
 import java.util.Scanner;
 
 public class EmpWageComp {
-
+	
 	static final int present_fulltime = 1;
 	static final int present_parttime = 2;
+	
+	private int total_working_days;
+	private int total_working_hours;
+	private int perhour_wage;
+	private int total_wages;
+	
 	private static HashSet<EmpWageComp> set = new HashSet<EmpWageComp>();
 	
-	public void calculateWage(int total_working_days, int total_working_hours, int perhour_wage) {
+	public EmpWageComp(int total_working_days, int total_working_hours, int perhour_wage, int total_wages) {
+		super();
+		this.total_working_days = total_working_days;
+		this.total_working_hours = total_working_hours;
+		this.perhour_wage = perhour_wage;
+		this.total_wages = total_wages;
+	}
+	
+	public void calculateWage() {
 		int work_hours = 0;	
-		int total_wages = 0, total_hours = 0, total_days = 0;	
+		int total_hours = 0, total_days = 0;	
 		
 		while (total_days < total_working_days && total_hours <= total_working_hours)
 		{
@@ -32,8 +46,9 @@ public class EmpWageComp {
 
 			System.out.println("Current Day Hours: " + work_hours+ "   Total hours: " + total_hours);
 		}
-
-		System.out.println("The Total Monthly wages are " + total_hours * perhour_wage);
+		
+		total_wages = total_hours * perhour_wage;
+		System.out.println("The Total Monthly wages are " + total_wages);
 		
 	}
 	public static void main(String args[]){
@@ -53,8 +68,8 @@ public class EmpWageComp {
 				System.out.println("Enter the wage per hour:");
 				int ph_wage = sc.nextInt();
 				
-				EmpWageComp comp1= new EmpWageComp();
-				comp1.calculateWage(t_days,t_hrs,ph_wage);
+				EmpWageComp comp1= new EmpWageComp(t_days,t_hrs,ph_wage,0);
+				comp1.calculateWage();
 				set.add(comp1);
 			}
 		}
